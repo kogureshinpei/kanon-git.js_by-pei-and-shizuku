fee24fc69132ae659ff7d20a09536e1401380932
kogure shinpei <z01035265@gmail.com>
2025-12-19 10:57:37 +0900

    splideの実装とstar.htmlを作成しました

--- asset/scripts/script.js
+++ asset/scripts/script.js
@@ -39,3 +39,13 @@ but1.addEventListener('click',() => display());
 but2.addEventListener('click',() => create_text());
 but3.addEventListener('click',() => create_element());
 name_button.addEventListener('click',() => output(name_input));
+
+//--------splide-----------
+new Splide("#splide", {
+    autoplay: true, // 自動再生
+    type: "loop", // ループ
+    pauseOnHover: false, // カーソルが乗ってもスクロールを停止させない
+    pauseOnFocus: false, // 矢印をクリックしてもスクロールを停止させない
+    interval: 3000, // 自動再生の間隔
+    speed: 1000, // スライダーの移動時間
+  }).mount();
